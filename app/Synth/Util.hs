005636f7e32a8d05661b82b9acc13b0e2de4e399
casperrr <casper.sebastien@gmail.com>
2025-10-25 16:03:13 +0100

    added wav file exporting

--- app/Synth/Util.hs
+++ app/Synth/Util.hs
@@ -1,11 +1,13 @@
 module Synth.Util where
 
-import Data.ByteString.Builder ( floatLE, toLazyByteString )
+import Data.ByteString.Builder ( floatLE, toLazyByteString, string8, word32LE, word16LE, int16LE )
 import Data.ByteString.Lazy (writeFile)
 import Prelude hiding (writeFile)
+import Data.Int ( Int16 )
+import Data.Word ( Word32 )
 
 import Synth.Types
-import Data.Int ( Int16 )
+import Synth.Constants
 
 ------- Utility Functions -------
 
@@ -28,3 +30,32 @@ saveRaw path audio = writeFile path $ toLazyByteString $ foldMap floatLE audio
 
 -- to play audio run:
 -- ffplay -showmode 1 -f f32le -ar sampleRate "output.bin
+saveWav :: FilePath -> Audio -> IO ()
+saveWav filePath samples = writeFile filePath $ toLazyByteString builder
+    where
+        builder = mconcat [
+            ---- RIFF Header ----
+            string8 "RIFF", -- ChunkID
+            word32LE (4 + (8 + 16) + (8 + dataBytes)), -- ChunkSize, Overall size of file 32-Int
+            string8 "WAVE", -- Format
+            ---- Format Chunk ----
+            string8 "fmt ", -- Subchunk1ID
+            word32LE 16, -- Subchunk1Size, PCM = 16
+            word16LE 1, -- AudioFormat, PCM = 1
+            word16LE 1, -- NumChannels, Mono = 1
+            word32LE sr, -- SampleRate
+            word32LE (sr * (16 `div` 8)), -- ByteRate = SampleRate * NumChannels * BitsPerSample/8
+            word16LE (16 `div` 8), -- BlockAlign = NumChannels * BitsPerSample/8
+            word16LE 16, -- BitsPerSample
+            ---- Data Chunk ----
+            string8 "data", -- Subchunk2ID
+            word32LE dataBytes, -- Subchunk2Size = NumSamples * NumChannels * BitsPerSample/8
+            foldMap (int16LE . floatToInt16) samples -- Actual sound data
+         ]
+
+        -- The size of the data section in bytes
+        dataBytes :: Word32
+        dataBytes = fromIntegral $ length samples * (16 `div` 8)
+
+        sr :: Word32
+        sr = round sampleRate
