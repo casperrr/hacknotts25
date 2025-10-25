45319dd5f6b45a6343e07eba4078e80be203176d
Oliver Fines <oli.fines@hotmail.co.uk>
2025-10-25 16:03:12 +0100

    Add sawtooth and triangle wave

--- app/Synth/WaveForm.hs
+++ app/Synth/WaveForm.hs
@@ -7,3 +7,12 @@ sinWave = sin
 
 squareWave :: WaveForm
 squareWave t = if sin t >= 0 then 1 else -1
+
+sawtoothWave :: WaveForm
+sawtoothWave t = 2 * (t - fromIntegral (floor (1/2 + t)))
+
+triangleWave :: WaveForm
+triangleWave t = 2 * abs (2 * (t - fromIntegral (floor (t + 1/2)))) - 1
+
+noiseWave :: WaveForm
+noiseWave t = undefined
