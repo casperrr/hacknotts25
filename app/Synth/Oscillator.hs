88da77df17457251223a3b74b6b09b34b6d3cc54
Oliver Fines <oli.fines@hotmail.co.uk>
2025-10-25 19:45:52 +0100

    Add bullshits

--- app/Synth/Oscillator.hs
+++ app/Synth/Oscillator.hs
@@ -2,8 +2,22 @@ module Synth.Oscillator where
 
 import Synth.Types
 import Synth.Constants
+import Synth.WaveForm
+
 
 oscillator :: Hz -> WaveForm -> Seconds -> Audio
 oscillator hz wf t = map (wf . (*step)) [0.0 .. sampleRate * t]
     where
         step = (2 * pi * hz) / sampleRate
+
+combineWave :: Audio -> Audio -> Audio
+combineWave = zipWith (\a b -> max (-1) (min 1 ((a + b) / 2)))
+
+bullshit :: Audio
+bullshit = combineWave (oscillator pitchStd noiseWave 1) (oscillator pitchStd sinWave 1) ++ combineWave (oscillator (pitchStd*2) noiseWave 1) (oscillator (pitchStd*2) sinWave 1)
+
+bullshit2 :: Audio
+bullshit2 = combineWave (oscillator pitchStd sinWave 2) (oscillator pitchStd sinWave 1)
+
+bullshit3 :: Audio
+bullshit3 = oscillator pitchStd warmSynth 2
--- app/Synth/WaveForm.hs
+++ app/Synth/WaveForm.hs
@@ -18,7 +18,19 @@ triangleWave t = 2 * abs (2 * (t - fromIntegral (floor (t + 1/2)))) - 1
 noiseWave :: WaveForm
 noiseWave t = fst (randomFloat t)
 
--- The seed is currently based on t, which is probably very bad but it kind of works!
+{-
+The seed is currently based on t, which is probably very bad but it kind of
+works!
+
+Also this "random noise" uses a pseudorandom number generator so it will
+always have the same value for each sample taken at the same time
+-}
 randomFloat :: Float -> (Float, StdGen)
 randomFloat t = randomR (-1.0 :: Float, 1.0 :: Float) gen
                 where gen = mkStdGen (round ((t * t) / 67))
+
+pianoWave :: WaveForm
+pianoWave t = - (1 / 4 * sin (3 * pi * t)) + 1 / 4 * sin (pi * t) + sqrt 3 / 2 * cos (pi * t)
+
+warmSynth :: WaveForm
+warmSynth t = 0.5 * sinWave t + 0.3 * sawtoothWave t + 0.2 * triangleWave t
